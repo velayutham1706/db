96fcb7ef9b8e59a52e800df76c32189f9a7356e5
velayutham <velayutham1706@gmail.com>
2026-05-01 10:21:43 +0530

    Auth domain added

--- api/firebase-config.js
+++ api/firebase-config.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   const origin = req.headers.origin;
   const allowed = [
-    'https://yourapp.vercel.app',
+    'https://db-hifi.vercel.app',
     'http://localhost:3000'
   ];
   
@@ -11,7 +11,7 @@ export default function handler(req, res) {
 
   res.json({
     apiKey:            process.env.FIREBASE_API_KEY,
-    authDomain:        process.env.FIREBASE_AUTH_DOMAIN,
+    authDomain:        'db-hifi.vercel.app',
     projectId:         process.env.FIREBASE_PROJECT_ID,
     storageBucket:     process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
